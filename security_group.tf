61e6aaa5fc04958438f1e23230be1e51b6315e45
alexey <alexey@digger.dev>
2023-07-05 17:14:11 +0100

    Revert "switch to inline rules"
    This reverts commit a74b4004c21fbd72a6d466820f170674c88c6c79.

--- security_group.tf
+++ security_group.tf
@@ -3,25 +3,38 @@ resource "aws_security_group" "bastion_sg" {
   description = "Allow SSH access to bastion host and outbound internet access"
   vpc_id      = local.vpc_id
 
-  ingress {
-    description      = "ssh connection to bastion"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks       = var.allowed_hosts
-    security_group_id = aws_security_group.bastion_sg.id
-  }
-  egress {
-    protocol          = "-1"
-    from_port         = 0
-    to_port           = 0
-    cidr_blocks       = ["0.0.0.0/0"]
-    security_group_id = aws_security_group.bastion_sg.id
-  }
   lifecycle {
     create_before_destroy = true
-    ignore_changes = [ingress]
+    ignore_changes = ["ingress"]
   }
 
   tags = var.tags
 }
+
+resource "aws_security_group_rule" "bastion_sg_ssh_rule" {
+  protocol          = "TCP"
+  from_port         = 22
+  to_port           = 22
+  type              = "ingress"
+  cidr_blocks       = var.allowed_hosts
+  security_group_id = aws_security_group.bastion_sg.id
+}
+
+resource "aws_security_group_rule" "bastion_sg_internet_rule" {
+  protocol          = "-1"
+  from_port         = 0
+  to_port           = 0
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.bastion_sg.id
+}
+
+resource "aws_security_group_rule" "bastion_sg_intranet_rule" {
+  protocol          = "-1"
+  from_port         = 0
+  to_port           = 0
+  type              = "egress"
+  cidr_blocks       = var.internal_networks
+  security_group_id = aws_security_group.bastion_sg.id
+}
+
