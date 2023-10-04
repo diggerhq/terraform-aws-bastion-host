429e9a684608d8bf9a3612180e14a837a06046be
Alexey Skriptsov <veziak@yahoo.com>
2023-10-04 17:57:19 +0100

    Update bastion_host.tf

--- bastion_host.tf
+++ bastion_host.tf
@@ -1,10 +1,15 @@
 data "aws_ami" "amazon_linux_ami" {
   owners      = ["amazon"]
   most_recent = true
-
+  
   filter {
     name   = "name"
-    values = ["ami-0fc5d935ebf8bc3bc"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
